f5409c077eef71804572997bff8d150aa4619e63
0xD3struction <148156846+0xD3struction@users.noreply.github.com>
2025-01-30 00:02:59 +0100

    Create 5_Change_WinName.c

---
+++ PRANK APPS/1_CHANGE_EVERY_WINDOW_NAME/5_Change_WinName.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+BOOL CALLBACK EnWinChld(HWND ChldHandle, LPARAM lPar)
+{
+	int WinLen;
+	WinLen = GetWindowTextLengthA(ChldHandle)+1;
+	
+	printf ("\t| winlen: %d\n",WinLen++);
+	char * WinText = malloc(WinLen);
+	
+	GetWindowText(ChldHandle,WinText,WinLen);
+	if(IsWindowVisible(ChldHandle))
+	{
+	  printf ("\t| text: %s\n\n",WinText);
+	  
+	  SetWindowText(ChldHandle,"HaCkeD By 0xD3struction");
+	}
+	
+	free(WinText);
+}
+
+BOOL CALLBACK EnWinProc(HWND WinHandle, LPARAM lPar)
+{
+	int WinLen;
+	WinLen = GetWindowTextLengthA(WinHandle)+1;
+	
+	printf ("winlen: %d\n",WinLen++);
+	
+	char * WinText = malloc(WinLen);
+	GetWindowText(WinHandle,WinText,WinLen);
+	
+	if(IsWindowVisible(WinHandle))
+	{ 
+	  printf ("text: %s\n\n",WinText);
+	  SetWindowText(WinHandle,"HaCkeD By 0xD3struction");
+	  EnumChildWindows(WinHandle,&EnWinChld,0);
+	  
+	}
+	
+	free(WinText);
+}
+
+int main(void)
+{
+  while(1)
+  {
+	EnumWindows(&EnWinProc,0);
+	Sleep(500);
+}
+	system("pause");
+	return 0;
+}
